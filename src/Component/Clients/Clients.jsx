ed0a400352b6437ba0a32ab75d769df9e7cb8d04
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-09-30 15:57:43 +0530

    Client Page set

--- src/Component/Clients/Clients.jsx
+++ src/Component/Clients/Clients.jsx
@@ -2,29 +2,29 @@ import { useEffect } from "react";
 import "./Clients.css";
 import Home from "../Home/Home";
 import Client from "./ClientsData"
-const Clients =() =>{
+const Clients = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-    return(
+    return (
         <>
-            <Home/>
+            <Home />
             <div className="sectionHeading">
-            <h2>
-                Our Clients
-            </h2>
+                <h2>
+                    Our Clients
+                </h2>
             </div>
             <div className="ClientsContainer">
-                        {Client.map((item, index) => (
-                            <div
-                                id="ImageContainerLogo"
-                                key={index}
-                                data-aos="fade-up"
-                            >
-                                <img src={item.img} alt="" />
-                            </div>
-                        ))}
+                {Client.map((item, index) => (
+                    <div
+                        id="ImageContainerLogo"
+                        key={index}
+                        data-aos="fade-up"
+                    >
+                        <img src={item.img} alt="" />
                     </div>
+                ))}
+            </div>
         </>
     )
 }
