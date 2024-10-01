6197ea10f29693e9937b165b8d29671e39187f76
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-10-01 11:29:46 +0530

    Client Changes

--- src/Component/Clients/Clients.jsx
+++ src/Component/Clients/Clients.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./Clients.css";
 import Home from "../Home/Home";
 import Client from "./ClientsData"
+import { Row,Col } from "antd";
 const Clients = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -10,25 +11,32 @@ const Clients = () => {
         <>
             <Home />
             <br /><br />
-           <div className="ClientContainerSet">
-           <div className="sectionHeading">
-                <h2>
-                    Our Clients
-                </h2>
+            <div className="ClientContainerSet">
+                <div className="sectionHeading">
+                    <h2>
+                        Our Clients
+                    </h2>
+                </div>
+                <br /><br />
+                <div className="ClientsContainer">
+                    <Row>
+                        {Client.map((item, index) => (
+
+                            <Col lg={4} md={6} sm={24}>
+                                <div
+                                    id="ImageContainerLogo"
+                                    key={index}
+                                    data-aos="fade-up"
+                                >
+                                    <img src={item.img} alt="" />
+                                </div>
+                            </Col>
+                        ))}
+
+                    </Row>
+
+                </div>
             </div>
-            <br /><br />
-            <div className="ClientsContainer">
-                {Client.map((item, index) => (
-                    <div
-                        id="ImageContainerLogo"
-                        key={index}
-                        data-aos="fade-up"
-                    >
-                        <img src={item.img} alt="" />
-                    </div>
-                ))}
-            </div>
-           </div>
         </>
     )
 }
