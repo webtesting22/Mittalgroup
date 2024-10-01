6bc09c7fa63dfe5a680a3329c26fd62dc68be640
Megh39 <meghnanavati1@gmail.com>
2024-10-01 12:53:33 +0530

    added google map link

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="FooterContainer">
                     <div>
                         <p style={{color:"rgb(255, 204, 0)"}}>Regd. Office</p>
-                        <p>1, Sona Roopa Apartment, Opp.Lal Bunglow, C. G. Road, Ahmedabad - 380 009</p>
+                        <p><a href="https://maps.app.goo.gl/aaMEv5Q8KSG696N69">1, Sona Roopa Apartment, Opp.Lal Bunglow, C. G. Road, Ahmedabad - 380 009</a></p>
                         <p><b>Phone:</b> <a href="tel:+917926405484">+91-7926405484</a> , <a href="tel:26406484">26406484</a> , <a href="tel:09909905484">099099 05484</a></p>
                         <p><b>Email:</b><a href="mailto:mittalsection@gmail.com">mittalsection@gmail.com</a></p>
                     </div>
