3910a084ca6440b6218d36354adb877a73838b27
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-10-01 12:40:35 +0530

    Footer Link

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -6,7 +6,12 @@ const Footer = () => {
         <>
             <section style={{ backgroundColor: "black" }}>
                 <div className="FooterContainer">
-                    <p>Copyright © 2024 <span style={{color:"#ffcc00"}}>A B Mittal Group</span>, All Rights Reserved. Designed & Developed by : <Link to="https://www.outleadsolutions.com/" target="_blank">Outlead Solution</Link></p>
+                    <div>
+                        <p style={{color:"rgb(255, 204, 0)"}}>Regd. Office</p>
+                        <p>1, Sona Roopa Apartment, Opp.Lal Bunglow, C. G. Road, Ahmedabad - 380 009</p>
+                        <p><b>Ph:</b> +91 79 26405484, 26406484, <b>Fax:</b> +91 79 26465484, <b>Mo:</b> 099099 05484</p>
+                    </div>
+                    <p>Copyright © 2024 <span style={{ color: "#ffcc00" }}>A B Mittal Group</span>, All Rights Reserved. Designed & Developed by : <Link to="https://www.outleadsolutions.com/" target="_blank">Outlead Solution</Link></p>
                 </div>
             </section>
         </>
