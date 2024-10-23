05569e3ae3233b61ccb7215a2b0abadc33746268
webtesting22 <webtesting2232@gmail.com>
2024-10-23 12:24:57 +0530

    Required Contact us Changes

--- src/Component/ContactUs/ContactUs.jsx
+++ src/Component/ContactUs/ContactUs.jsx
@@ -20,12 +20,14 @@ const ContactUs = () => {
   return (
 
     <>
-      <br /><br /><br />
       <div className='contactContainer'>
-        <h1 className='contactTitle'>CONTACT US</h1>
-        <p className='contactPara'>To learn more about our products, services, or potential partnerships, reach out to us.
-          Together, we can build a stronger, more sustainable future for the industries that rely on us.</p>
-          
+        <div className='HeadingContainer'>
+          <h1 >CONTACT US</h1>
+          <p >To learn more about our products, services, or potential partnerships, reach out to us.
+            Together, we can build a stronger, more sustainable future for the industries that rely on us.</p>
+        </div>
+
+        <br /><br />
         <div className='map'>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7343.67102500053!2d72.5537858339964!3d23.02981095646011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84f368a87e9b%3A0x2f2116eeb183aab6!2sMittal%20Sections%20Limited!5e0!3m2!1sen!2sin!4v1729144211887!5m2!1sen!2sin"
             style={{ width: "100%", height: "400px", border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -33,23 +35,23 @@ const ContactUs = () => {
         <Row className='contact-details-row'>
           <Col lg={12} xs={24} >
             <div className="contact-details">
-              <h1>Contact Details</h1>
-              <p className="address"><b>Head Office: </b><a href="https://maps.app.goo.gl/rfcWFEJZN66dkx7YA" target="_blank">1, Sona Roopa, Opp. Lal Bungalow, C.G. Road, Ahmedabad -380009.</a></p>
-              <p className="address"><b>Unit I: </b><a href="https://maps.app.goo.gl/jLJzi4Veib8yfsAA7" target="_blank">14, Changodar Industrial Estate, B/H Trivedi Marble, Sarkhej- Bavla Highway, Changodar, Ahmedabad -382213.</a></p>
-              <p className="address"><b>Unit II: </b><a href="https://maps.app.goo.gl/jLJzi4Veib8yfsAA7" target="_blank">23, Changodar Industrial Estate, B/H Trivedi Marble, Sarkhej- Bavla Highway, Changodar, Ahmedabad -382213.</a></p>
+              <h2>Contact Details</h2>
+              <p ><b>Head Office: </b><a href="https://maps.app.goo.gl/rfcWFEJZN66dkx7YA" target="_blank">1, Sona Roopa, Opp. Lal Bungalow, C.G. Road, Ahmedabad -380009.</a></p>
+              <p ><b>Unit I: </b><a href="https://maps.app.goo.gl/jLJzi4Veib8yfsAA7" target="_blank">14, Changodar Industrial Estate, B/H Trivedi Marble, Sarkhej- Bavla Highway, Changodar, Ahmedabad -382213.</a></p>
+              <p ><b>Unit II: </b><a href="https://maps.app.goo.gl/jLJzi4Veib8yfsAA7" target="_blank">23, Changodar Industrial Estate, B/H Trivedi Marble, Sarkhej- Bavla Highway, Changodar, Ahmedabad -382213.</a></p>
               <p>
-              <b>Phone: </b><a href='tel:9909905484'>9909905484</a>
-              {/* <b>Phone:</b><a href="tel:+917926405484">+917926405484</a>, */}
+                <b>Phone: </b><a href='tel:9909905484'>9909905484</a>
+                {/* <b>Phone:</b><a href="tel:+917926405484">+917926405484</a>, */}
                 {/* <a href="tel:+917926406484">+917926406484</a> */}
               </p>
               <p><b>Mail: </b><a href="mailto:mittalsection@gmail.com">mittalsection@gmail.com</a></p>
             </div>
           </Col>
-          <Col lg={1} md={0}/>
+          <Col lg={1} md={0} />
           <Col lg={11} xs={24}>
             <div className="contact-form">
-              <h1>Reach Out To Us!</h1>
-
+              <h2>Reach Out To Us!</h2>
+<br />
               <Form
                 name="basic"
                 labelCol={{
@@ -60,7 +62,7 @@ const ContactUs = () => {
                 }}
                 style={{
                   maxWidth: 600,
-                  
+
                 }}
                 initialValues={{
                   remember: true,
@@ -73,7 +75,7 @@ const ContactUs = () => {
                   label="Your Name:"
                   name="name"
                   style={{
-                    textAlign:"left"
+                    textAlign: "left"
                   }}
                   rules={[
                     {
