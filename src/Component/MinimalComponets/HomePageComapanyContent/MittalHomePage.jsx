90b67b43dafaeae28000a0a0a20c8350fc1907eb
webtesting22 <webtesting2232@gmail.com>
2024-10-25 16:00:11 +0530

    Images Updated Remove

--- src/Component/MinimalComponets/HomePageComapanyContent/MittalHomePage.jsx
+++ src/Component/MinimalComponets/HomePageComapanyContent/MittalHomePage.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Row, Col, Image, Button } from "antd";
 import "./MittalSectionHomePageContainer.css"
-import FlatBars1 from "./FlatBars1.jpeg"
-import RoundBars1 from "./RoundBars1.jpeg"
-import ChannelsBar1 from "./ChannelsBar1.jpeg"
-import Angles1 from "./Angles1.jpeg"
 import { Link } from "react-router-dom";
 import ClientHome from "../ClientHome/ClientHome";
 const MittalHomePage = () => {
@@ -16,7 +12,7 @@ const MittalHomePage = () => {
             cardDescription: "Steel flat bars are durable, rectangular-shaped metal bars used in construction and fabrication. They offer strength and versatility for structural supports and custom projects.",
             CardImages: [
                 {
-                    img: FlatBars1
+                    img: "https://webtesting-upload.vercel.app/assets/FlatBars1-DOa6N3xA.jpeg"
                 },
 
             ]
@@ -27,7 +23,7 @@ const MittalHomePage = () => {
             cardDescription: "Steel round bars are strong, cylindrical rods commonly used in construction, machining, and industrial applications. They provide excellent durability and precision for shafts, supports, and custom metalwork.",
             CardImages: [
                 {
-                    img: RoundBars1
+                    img: "https://webtesting-upload.vercel.app/assets/RoundBars1-CGZDPqFq.jpeg"
                 },
 
             ]
@@ -38,7 +34,7 @@ const MittalHomePage = () => {
             cardDescription: "Steel channels are U-shaped metal profiles widely used in construction, framing, and structural support. They offer strength and stability for load-bearing applications and custom fabrication projects.",
             CardImages: [
                 {
-                    img: ChannelsBar1
+                    img: "https://webtesting-upload.vercel.app/assets/ChannelsBar1-BN9_sASO.jpeg"
                 },
 
             ]
@@ -49,7 +45,7 @@ const MittalHomePage = () => {
             cardDescription: "Steel angles are L-shaped metal bars used for structural reinforcement and framing in construction and engineering projects. They provide strength, stability, and versatility for various applications.",
             CardImages: [
                 {
-                    img: Angles1
+                    img: "https://webtesting-upload.vercel.app/assets/Angles1-B8UcZCAq.jpeg"
                 },
 
             ]
--- src/Component/Mittalgallery/MittalGallery.jsx
+++ src/Component/Mittalgallery/MittalGallery.jsx
@@ -1,34 +1,10 @@
-import React, { useEffect } from "react";
-import { Row, Col, Image } from "antd";
-// import mittalImage1 from "./1-min.jpg";
-// import mittalImage2 from "./2-min.jpg";
-// import mittalImage3 from "./3-min.jpg";
-// import mittalImage4 from "./4-min.jpg";
-// import mittalImage5 from "./5-min.jpg";
-// import mittalImage6 from "./6-min.jpg";
-// import mittalImage7 from "./7-min.jpg";
-// import mittalImage8 from "./8-min.jpg";
-// import mittalImage9 from "./9-min.jpg";
-// import mittalImage10 from "./10-min.jpg";
-// import mittalImage11 from "./11-min.jpg";
+import React from "react";
+import { Row, Col,Image } from "antd";
 import "./Mittalgallery.css"
+
+
 const MittalGallery = () => {
-    // useEffect(() => {
-    //     window.scrollTo(0, 0);
-    // }, []);
-    // const MittalGalleryImages = [
-    //     { image: mittalImage1 },
-    //     { image: mittalImage2 },
-    //     { image: mittalImage3 },
-    //     { image: mittalImage4 },
-    //     { image: mittalImage5 },
-    //     { image: mittalImage6 },
-    //     { image: mittalImage7 },
-    //     { image: mittalImage8 },
-    //     { image: mittalImage9 },
-    //     { image: mittalImage10 },
-    //     { image: mittalImage11 }
-    // ];
+
     const MittalGalleryImages = [
         { image: "https://webtesting-upload.vercel.app/assets/1-min-BnI64RRP.jpg" },
         { image: "https://webtesting-upload.vercel.app/assets/2-min-atdJh0He.jpg" },
@@ -46,9 +22,7 @@ const MittalGallery = () => {
     return (
         <>
             <section id="MittalGalleryContainer">
-                {/* <div className="productTitle">
-                    <h2>Our Gallery</h2>
-                </div> */}
+
                 <div className="HeadingContainer">
                     <h2 data-aos="fade-up" data-aos-duration="1000">Our Gallery</h2>
                     <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">Crafting Strength Through Steel Excellence</p>
@@ -58,8 +32,8 @@ const MittalGallery = () => {
                 <Row gutter={[16, 16]}>
                     {MittalGalleryImages.map((item, index) => (
                         <Col key={index} xs={24} sm={24} md={12} lg={8}>
-                            <div className="gallery-image-container">
-                                <img
+                            <div>
+                                <Image
                                     src={item.image}
                                     alt={`Mittal Gallery Image ${index + 1}`}
                                     className="Mittal-gallery-image"
