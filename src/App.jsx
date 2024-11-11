f4e18bd53a832c843e743bb9740cba0b29fcebae
Megh39 <meghnanavati1@gmail.com>
2024-11-11 10:57:25 +0530

    gallery check

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { Route, Routes } from 'react-router'
 import AboutUs from './Component/AboutUs/AboutUs'
 import Group from './Component/GroupCompanies/Group'
 import ContactUs from './Component/ContactUs/ContactUs'
+// import MSLGallery from './Component/Mittalgallery/Mittalgallery'
 import MSLGallery from './Component/Mittalgallery/Mittalgallery'
+
 import Footer from "./Component/Footer/Footer"
 import MSLHome from './Component/MittalHome/MittalHome'
 import Clients from "./Component/Clients/Clients"
