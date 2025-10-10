3f5b41d95508d91f75ce6bf4a03e7b0176ad7638
Neel Patel <neeelpatell19@gmail.com>
2025-10-10 15:29:42 +0530

    fix: video updated

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './Home.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay } from 'swiper/modules';
-import MSLHomePage from '../MinimalComponets/HomePageComapanyContent/MittalHomePage';
-import NumbersComponent from '../MinimalComponets/NumbersComponent/NumbersComponent';
-import ClientHome from '../MinimalComponets/ClientHome/ClientHome';
+import React, { useEffect, useState, useRef } from "react";
+import "./Home.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay } from "swiper/modules";
+import MSLHomePage from "../MinimalComponets/HomePageComapanyContent/MittalHomePage";
+import NumbersComponent from "../MinimalComponets/NumbersComponent/NumbersComponent";
+import ClientHome from "../MinimalComponets/ClientHome/ClientHome";
 const Home = ({ images = [] }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [fadeIn, setFadeIn] = useState(true); // State to control fading
@@ -31,27 +31,25 @@ const Home = ({ images = [] }) => {
           );
           setFadeIn(true); // Start fading in the new image
         }, 500); // Timeout should match the CSS transition duration
-
       }, 2000); // Change every 2 seconds
 
       return () => clearInterval(interval); // Clean up the interval on unmount
     }
   }, [images]);
 
-
-
   return (
     <>
-      <div className='AnimatedHomeSlider'>
-        <div className='MSLHomeContent'>
-
+      <div className="AnimatedHomeSlider">
+        <div className="MSLHomeContent">
           <h2>Shaping the future of Structural Steel</h2>
-          <p>MSL provides premium steel products designed for strength and durability. Our precision engineering ensures reliable solutions for every project.</p>
+          <p>
+            MSL provides premium steel products designed for strength and
+            durability. Our precision engineering ensures reliable solutions for
+            every project.
+          </p>
         </div>
-        <div className='blackOverlay'>
-
-        </div>
-        <div className='pcNone'>
+        <div className="blackOverlay"></div>
+        <div className="pcNone">
           <Swiper
             slidesPerView={1}
             spaceBetween={30}
@@ -59,25 +57,28 @@ const Home = ({ images = [] }) => {
             speed={1000}
             modules={[Autoplay]}
             className="mySwiper"
-            id='SwiperHome'
+            id="SwiperHome"
             style={{ position: "static" }}
           >
             <SwiperSlide>
               <video
-                src="https://webtesting-upload.vercel.app/assets/AB%20Mittal%20Video-CSf5vDXC.mp4"
+                src="https://prepseed.s3.ap-south-1.amazonaws.com/ABmittal_1.mp4"
                 loop={true} // Set loop to true for continuous playback
                 autoPlay
                 muted={isMuted}
                 playsInline // Prevent fullscreen on mobile
                 ref={videoRef}
-
-
-                style={{ width: '100%', height: "100%", pointerEvents: 'none', objectFit: "cover" }} // Ensure no interaction triggers fullscreen
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  pointerEvents: "none",
+                  objectFit: "cover",
+                }} // Ensure no interaction triggers fullscreen
               />
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className='mobileNone'>
+        <div className="mobileNone">
           <video
             src="https://webtesting-upload.vercel.app/assets/AB%20Mittal%20Video-CSf5vDXC.mp4"
             loop={true} // Set loop to true for continuous playback
@@ -85,9 +86,7 @@ const Home = ({ images = [] }) => {
             muted={isMuted}
             playsInline // Prevent fullscreen on mobile
             ref={videoRef}
-
-
-            style={{ width: '100%', height: "100%", pointerEvents: 'none' }} // Ensure no interaction triggers fullscreen
+            style={{ width: "100%", height: "100%", pointerEvents: "none" }} // Ensure no interaction triggers fullscreen
           />
           <button
             onClick={toggleMute}
@@ -105,9 +104,13 @@ const Home = ({ images = [] }) => {
               zIndex: 100000, // Ensure button appears above the video
             }}
           >
-            {isMuted ? <i class='bx bx-volume-mute' ></i> : <i class='bx bx-volume-full'></i>}
-          </button>        </div>
-
+            {isMuted ? (
+              <i class="bx bx-volume-mute"></i>
+            ) : (
+              <i class="bx bx-volume-full"></i>
+            )}
+          </button>{" "}
+        </div>
       </div>
       <NumbersComponent />
       <MSLHomePage />
